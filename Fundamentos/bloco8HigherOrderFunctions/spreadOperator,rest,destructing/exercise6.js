6c451a8ffb819d32378ff71cb7c293c9f42031b3
vinisavordelli <vinicius.savordelli@gmail.com>
2021-10-08 17:16:52 -0300

    8.5

---
+++ Fundamentos/bloco8HigherOrderFunctions/spreadOperator,rest,destructing/exercise6.js
@@ -0,0 +1,9 @@
+// 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+// Dica: use array destructuring e abbreviation object literal .
+// Copiar
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+// escreva toObject abaixo
+const toObject = ([model, manufacturer, releaseYear]) => console.log({model, manufacturer, releaseYear});
+
