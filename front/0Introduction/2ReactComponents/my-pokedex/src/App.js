2b05b4eb708c858e7abb52d671391d4061a268d1
vinisavordelli <vinicius.savordelli@gmail.com>
2021-11-04 13:06:29 -0300

    exercicios dia 10.2

---
+++ front/0Introduction/2ReactComponents/my-pokedex/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import pokemons from './data';
+//Import do json criado com os pokemons e seus dados.
+import Pokedex from './Components/Pokedex';
+
+class App extends React.Component {
+render() {
+  return (
+    <div className="App">
+      <h1> Pokedex </h1>
+      <Pokedex pokemons={pokemons} />
+    </div>
+  );
+}
+
+}
+
+export default App;
