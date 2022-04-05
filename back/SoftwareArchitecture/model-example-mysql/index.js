a283fa8e1db6ac0ccb87e900149938231a1273e3
Vinicius <vinicius.savordelli@gmail.com>
2022-04-05 19:22:35 -0300

    adds a lot of exercises

--- back/SoftwareArchitecture/model-example-mysql/index.js
+++ back/SoftwareArchitecture/model-example-mysql/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 const Author = require('./models/author');
+app.use(bodyParser.json());
+
 
 app.get('/authors', async (req, res) => {
   const [authors] = await Author.getAll();
   res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const {id} = req.params;
+  const author = await Author.findById(id);
+  if(!author) return res.status(404).send('Author not found');
+  res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+      return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! '});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- back/SoftwareArchitecture/model-example-mysql/models/author.js
+++ back/SoftwareArchitecture/model-example-mysql/models/author.js
@@ -27,6 +27,31 @@ const getAll = async () => {
   return authors.map(serialize).map(getNewAuthor);
 }
 
+const findById = async (id) => {
+  const [author] = await connection.execute('select id, first_name, middle_name, last_name FROM authors WHERE id = ?', [id]);
+  if (author.length === 0) return null;
+  const {firstName, middleName, lastName} = author.map(serialize)[0];
+
+  return getNewAuthor({id, firstName, middleName, lastName});
+}
+
+const isValid = (firstName, middleName, lastName) => {
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+  if (middleName && typeof middleName !== 'string') return false;
+
+  return true;
+};
+
+const create = async (firstName, middleName, lastName) => connection.execute(
+  'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?,?,?)',
+  [firstName, middleName, lastName],
+);
+
+
 module.exports = {
   getAll,
+  findById,
+  isValid,
+  create,
 }
